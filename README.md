6c9981efdb285edb2f1b4308750f03181861f260
unknown lizzard <mateo.m.anceron@gmail.com>
2026-06-25 11:22:22 +0000

    new rocket builder+readme update

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 space/mission-control desktop.
 minimal space control inspired OS with mission logs and launch control!
 (WARNING: some buttons may actually launch a rocket)
+added 3 apps, artemis launch control, rocket build and mission log, they are all functional and fun to use
+make sure you don't launch too many rockets!
--- script.js
+++ script.js
@@ -9,19 +9,6 @@ setInterval(updateTime, 1000);
 var biggestIndex = 1;
 var selectedIcon = undefined;
 
-var welcomeScreen = document.querySelector("#welcome");
-var welcomeScreenClose = document.querySelector("#welcomeclose");
-var welcomeScreenOpen = document.querySelector("#welcomeopen");
-
-var missionScreen = document.querySelector("#mission");
-var missionClose = document.querySelector("#missionclose");
-var missionIcon = document.querySelector("#missionIcon");
-
-var launchScreen = document.querySelector("#launch");
-var launchClose = document.querySelector("#launchclose");
-var launchIcon = document.querySelector("#launchIcon");
-
-
 function closeWindow(element) {
   element.style.display = "none";
 }
@@ -45,50 +32,44 @@ function selectIcon(icon) {
   icon.classList.add("selected");
 }
 
+function setupWindow(windowId, closeId, openIconId) {
+  var windowElement = document.querySelector("#" + windowId);
+  var closeButton = document.querySelector("#" + closeId);
+  var openIcon = document.querySelector("#" + openIconId);
 
-welcomeScreenClose.addEventListener("click", function() {
-  closeWindow(welcomeScreen);
+  closeButton.addEventListener("click", function() {
+    closeWindow(windowElement);
+  });
+
+  openIcon.addEventListener("click", function() {
+    selectIcon(openIcon);
+    openWindow(windowElement);
+  });
+
+  windowElement.addEventListener("mousedown", function() {
+    bringToFront(windowElement);
+  });
+
+  dragElement(windowElement);
+}
+
+document.querySelector("#welcomeclose").addEventListener("click", function() {
+  closeWindow(document.querySelector("#welcome"));
 });
 
-welcomeScreenOpen.addEventListener("click", function() {
-  openWindow(welcomeScreen);
+document.querySelector("#welcomeopen").addEventListener("click", function() {
+  openWindow(document.querySelector("#welcome"));
 });
 
-missionClose.addEventListener("click", function() {
-  closeWindow(missionScreen);
+document.querySelector("#welcome").addEventListener("mousedown", function() {
+  bringToFront(document.querySelector("#welcome"));
 });
 
-missionIcon.addEventListener("click", function() {
-  selectIcon(missionIcon);
-  openWindow(missionScreen);
-});
+dragElement(document.querySelector("#welcome"));
 
-launchClose.addEventListener("click", function() {
-  closeWindow(launchScreen);
-});
-
-launchIcon.addEventListener("click", function() {
-  selectIcon(launchIcon);
-  openWindow(launchScreen);
-});
-
-
-welcomeScreen.addEventListener("mousedown", function() {
-  bringToFront(welcomeScreen);
-});
-
-missionScreen.addEventListener("mousedown", function() {
-  bringToFront(missionScreen);
-});
-
-launchScreen.addEventListener("mousedown", function() {
-  bringToFront(launchScreen);
-});
-
-
-dragElement(welcomeScreen);
-dragElement(missionScreen);
-dragElement(launchScreen);
+setupWindow("mission", "missionclose", "missionIcon");
+setupWindow("launch", "launchclose", "launchIcon");
+setupWindow("rocket", "rocketclose", "rocketIcon");
 
 
 function dragElement(element) {
@@ -97,7 +78,9 @@ function dragElement(element) {
   var currentX = 0;
   var currentY = 0;
 
-  document.getElementById(element.id + "header").onmousedown = startDragging;
+  var header = document.getElementById(element.id + "header");
+
+  header.onmousedown = startDragging;
 
   function startDragging(e) {
     e.preventDefault();
@@ -127,158 +110,59 @@ function dragElement(element) {
     document.onmousemove = null;
   }
 }
+var rocketParts = [];
 
+function addRocketPart(name, emoji) {
+  rocketParts.push({
+    name: name,
+    emoji: emoji
+  });
 
-var missions = [
-  {
-    title: "OrbitOS Boot",
-    date: "Day 1",
-    text: "The control center is online. Nobody pressed the launch button yet, which is honestly a miracle."
-  },
-  {
-    title: "Signal Found",
-    date: "Day 2",
-    text: "A weird signal appeared on the radar. It might be deep space noise. It might also be someone microwaving noodles next to the antenna."
-  },
-  {
-    title: "Artemis Warning",
-    date: "Day 3",
-    text: "Artemis V is armed, but the safety team placed a sticky note on the launch button. Mission control considers this secure."
-  },
-  {
-    title: "Artemis Launch",
-    date: "Day 4",
-    text: "The sticky note was ignored. The rocket launched!"
-  },
-  {
-    title: "Mun Landing",
-    date: "Day 5",
-    text: "The space probe successfully landed on the Mun!"
-  },
-  {
-    title: "Hack Club on the Mun",
-    date: "Day 6",
-    text: "The whole team arrived safely, except Jack, who tripped on a crater. He is unharmed."
-  }
-];
-
-function showMission(index) {
-  var mission = missions[index];
-
-  document.querySelector("#missionText").innerHTML =
-    "<h2>" + mission.title + "</h2>" +
-    "<p><strong>" + mission.date + "</strong></p>" +
-    "<p>" + mission.text + "</p>";
+  updateRocketPreview();
 }
 
-function loadMissionList() {
-  var list = document.querySelector("#missionList");
-  list.innerHTML = "";
+function updateRocketPreview() {
+  var preview = document.querySelector("#rocketPreview");
+  preview.innerHTML = "";
 
-  for (var i = 0; i < missions.length; i++) {
-    var item = document.createElement("div");
-    item.className = "missionItem";
-    item.innerHTML = "<strong>" + missions[i].title + "</strong><br>" + missions[i].date;
+  for (var i = 0; i < rocketParts.length; i++) {
+    var part = document.createElement("div");
+    part.className = "rocketPart";
+    part.innerHTML = rocketParts[i].emoji + " " + rocketParts[i].name;
 
-    let missionNumber = i;
-
-    item.addEventListener("click", function() {
-      showMission(missionNumber);
-    });
-
-    list.appendChild(item);
+    preview.appendChild(part);
   }
 
-  showMission(0);
-}
-
-loadMissionList();
-
-
-var fuel = 0;
-var armed = false;
-var launched = false;
-
-
-var BuildButton = document.querySelector("#buildButton");
-var fuelButton = document.querySelector("#fuelButton");
-var armButton = document.querySelector("#armButton");
-var launchButton = document.querySelector("#launchButton");
-var abortButton = document.querySelector("#abortButton");
-
-function updateLaunchPanel() {
-  document.querySelector("#fuelBar").style.width = fuel + "%";
-  document.querySelector("#fuelText").innerText = fuel + "%";
-
-  if (launched === true) {
-    document.querySelector("#launchStatus").innerText = "Launched";
-  } else if (armed === true) {
-    document.querySelector("#launchStatus").innerText = "Armed";
-  } else if (fuel >= 100) {
-    document.querySelector("#launchStatus").innerText = "Fueled";
+  if (rocketParts.length === 0) {
+    document.querySelector("#rocketStatus").innerText = "Not ready";
   } else {
-    document.querySelector("#launchStatus").innerText = "Not ready";
+    document.querySelector("#rocketStatus").innerText = "Building...";
   }
 }
 
-function addLaunchLog(text) {
-  var log = document.querySelector("#launchLog");
-  log.innerHTML = "<p>" + text + "</p>" + log.innerHTML;
-}
-
-fuelButton.addEventListener("click", function() {
-  if (launched === true) {
-    addLaunchLog("The rocket is already gone.");
-    return;
-  }
-
-  fuel = fuel + 20;
-
-  if (fuel > 100) {
-    fuel = 100;
-  }
-
-  addLaunchLog("Fuel loaded: " + fuel + "%");
-  updateLaunchPanel();
+document.querySelector("#fuselageButton").addEventListener("click", function() {
+  addRocketPart("Fuselage", "🟦");
 });
 
-armButton.addEventListener("click", function() {
-  if (fuel < 100) {
-    addLaunchLog("Cannot arm. Fuel is not full.");
-    return;
-  }
-
-  armed = true;
-  addLaunchLog("Rocket armed. Please do not sneeze near the red button.");
-  updateLaunchPanel();
+document.querySelector("#engineButton").addEventListener("click", function() {
+  addRocketPart("Engine", "🔥");
 });
 
-launchButton.addEventListener("click", function() {
-  if (armed === false) {
-    addLaunchLog("Launch denied. Rocket is not armed.");
-    return;
-  }
-
-  launched = true;
-  addLaunchLog("Artemis V launched. OrbitOS is now slightly more dangerous.");
-  updateLaunchPanel();
+document.querySelector("#parachuteButton").addEventListener("click", function() {
+  addRocketPart("Parachute", "🪂");
 });
 
-abortButton.addEventListener("click", function() {
-  if (launched === true) {
-    addLaunchLog("Abort denied. Rocket is already launched.");
-    return;
-  }
+document.querySelector("#podButton").addEventListener("click", function() {
+  addRocketPart("Pod", "👨‍🚀");
+});
 
-  armed = false;
-  fuel = 0;
-  addLaunchLog("Launch aborted. Rocket is now safe.");
-  updateLaunchPanel();
+document.querySelector("#clearRocketButton").addEventListener("click", function() {
+  rocketParts = [];
+  updateRocketPreview();
 });
-BuildButton.addEventListener("click", function() {
-  armed = false;
-  launched = false;
-  fuel = 0;
-  addLaunchLog("Rocket rebuilt. Artemis V is now safe.");
-  updateLaunchPanel();
+
+document.querySelector("#saveButton").addEventListener("click", function() {
+  document.querySelector("#rocketStatus").innerText = "Saved!";
 });
+
+updateRocketPreview();
